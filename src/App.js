3b14d0c24fdd5a51addd482fda92b7c2aeb07adc
SalmaMej <salma.mejdouba@gmail.com>
2020-12-01 16:48:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div class='form-div'>
+    <form>
+      <label for="fname">First name:</label>
+      <input type="text" id="fname" name="fname" class='form-inline forms' placeholder='Enter your first name'/>
+      <label for="lname">Last name:</label>
+      <input type="text" id="lname" name="lname" class='form-inline forms' placeholder='Enter your last name'/>
+      
+      <label for="fname">Email adress:</label>
+      <input type="email" name="email" class='form-inline forms' placeholder='abc@efg.com'/>
+      <label for="fname">Password:</label>
+      <input type="password" name="password" class='form-inline forms' placeholder='********'/>
+      <button type='submit' class="btn btn-danger btn-space" >Submit</button>
+    </form>
+  </div>
   );
 }
 
